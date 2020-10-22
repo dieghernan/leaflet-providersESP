2d8febf4b0facec833bd5c308d45b14b0d938e2d
dieghernan <diego.hernangomezherrero@gmail.com>
2020-10-22 19:21:22 +0200

    No draw

--- docs/js/viewer.js
+++ docs/js/viewer.js
@@ -30,8 +30,6 @@ map.locate({setView: true, maxZoom: 16});
 
 function onLocationFound(e) {
     var radius = e.accuracy*2;
-
-    L.circle(e.latlng, radius).addTo(map);
 }
 
 map.on('locationfound', onLocationFound);
