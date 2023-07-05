39c237c2ed11f0d2358b0c3c6f443d2f9a41c1e1
dieghernan <dieghernan@users.noreply.github.com>
2023-07-05 19:02:53 +0000

    jekyll build from Action 1d5fcbc5a46ad14e6e73d20aec91f3befbb2dfaa

--- assets/js/ch_ytdefer/ch_ytdefer.js
+++ assets/js/ch_ytdefer/ch_ytdefer.js
@@ -32,7 +32,6 @@ function ch_ytdefer_setup() {
     }
     if (typeof YT == "undefined") {
         var js = d.createElement("script");
-        js.type = "text/javascript";
         js.src = "https://www.youtube.com/player_api";
         d.body.appendChild(js);
     }
@@ -62,6 +61,7 @@ function gen_ytdefer_clk(i) {
         var ifr = d.createElement("div");
         ifr.id = "ch_ytdefer_frame" + i;
         ifr.classList.add("embed-responsive-item");
+        ifr.setAttribute("itemprop", "embedUrl");
         im.appendChild(ifr);
 
         //Remove attributes
@@ -79,3 +79,5 @@ function gen_ytdefer_clk(i) {
 
     };
 }
+
+window.addEventListener('load', ch_ytdefer_setup);
